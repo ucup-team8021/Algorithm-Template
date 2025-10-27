e123546a5dab66c0d3e4ae33599f2c6d5808d4b4
lotus-grass <lotus_grass@outlook.com>
2025-10-27 19:27:42 +0800

    upd bignum

--- 数学Math/bignum.cpp
+++ 数学Math/bignum.cpp
@@ -1,99 +1,58 @@
 // {"name": "高精度计算", "intro": "对大整数进行计算。"}
-#include <cstdio>
-#include <cstring>
-constexpr int LEN = 1004;
-int a[LEN], b[LEN], c[LEN], d[LEN];
-void clear(int a[]) {
-    for (int i = 0; i < LEN; ++i) a[i] = 0;
-}
-void read(int a[]) {
-    static char s[LEN + 1];
-    scanf("%s", s);
-    clear(a);
-    int len = strlen(s);
-    for (int i = 0; i < len; ++i) a[len - i - 1] = s[i] - '0';
-}
-void print(int a[]) {
-    int i;
-    for (i = LEN - 1; i >= 1; --i)
-        if (a[i] != 0) break;
-    for (; i >= 0; --i) putchar(a[i] + '0');
-    putchar('\n');
-}
-void add(int a[], int b[], int c[]) {
-    clear(c);
-    for (int i = 0; i < LEN - 1; ++i) {
-        c[i] += a[i] + b[i];
-        if (c[i] >= 10) {
-            c[i + 1] += 1;
-            c[i] -= 10;
-        }
-    }
-}
-void sub(int a[], int b[], int c[]) {
-    clear(c);
-    for (int i = 0; i < LEN - 1; ++i) {
-        c[i] += a[i] - b[i];
-        if (c[i] < 0) {
-            c[i + 1] -= 1;
-            c[i] += 10;
-        }
-    }
-}
-void mul(int a[], int b[], int c[]) {
-    clear(c);
-    for (int i = 0; i < LEN - 1; ++i) {
-        for (int j = 0; j <= i; ++j) c[i] += a[j] * b[i - j];
-        if (c[i] >= 10) {
-            c[i + 1] += c[i] / 10;
-            c[i] %= 10;
-        }
-    }
-}
-bool greater_eq(int a[], int b[], int last_dg, int len) {
-    if (a[last_dg + len] != 0) return true;
-    for (int i = len - 1; i >= 0; --i) {
-        if (a[last_dg + i] > b[i]) return true;
-        if (a[last_dg + i] < b[i]) return false;
-    }
-    return true;
-}
-void div(int a[], int b[], int c[], int d[]) {
-    clear(c), clear(d);
-    int la, lb;
-    for (la = LEN - 1; la > 0; --la)
-        if (a[la - 1] != 0) break;
-    for (lb = LEN - 1; lb > 0; --lb)
-        if (b[lb - 1] != 0) break;
-    if (lb == 0) {
-        puts("> <");
-        return;
-    }
-    for (int i = 0; i < la; ++i) d[i] = a[i];
-    for (int i = la - lb; i >= 0; --i) {
-        while (greater_eq(d, b, i, lb)) {
-            for (int j = 0; j < lb; ++j) {
-                d[i + j] -= b[j];
-                if (d[i + j] < 0) {
-                    d[i + j + 1] -= 1;
-                    d[i + j] += 10;
-                }
-            }
-            c[i] += 1;
-        }
-    }
-}
-int main() {
-    read(a);
-    char op[4];
-    scanf("%s", op);
-    read(b);
-    switch (op[0]) {
-    case '+': add(a, b, c), print(c); break;
-    case '-': sub(a, b, c), print(c); break;
-    case '*': mul(a, b, c), print(c); break;
-    case '/': div(a, b, c, d), print(c), print(d); break;
-    default: puts("> <");
-    }
-    return 0;
-}
+#include <bits/stdc++.h>
+using namespace std;
+class BINT 
+{         
+private:                          
+ string s;
+public:                              
+ BINT(string x = "0"): s(x) {};   
+ const BINT operator+(const BINT &t) { 
+  string sa = s, sb = t.s;
+  int la = sa.size(), lb = sb.size();
+  int m = max(la, lb);         
+  vector<int> a(m + 1, 0), b(m + 1, 0), c(m + 1, 0);
+  for (int i = 0; i < la; i++) a[i] = sa[la - 1 - i] - '0';
+  for (int i = 0; i < lb; i++) b[i] = sb[lb - 1 - i] - '0';
+  for (int i = 0; i < m; i++) c[i] += a[i] + b[i], c[i + 1] += c[i] / 10, c[i] = c[i] % 10;
+  while (!c[m] && m) m--;    
+  string res;
+  for (int i = m; i >= 0; i--) res += char(c[i] + '0');
+  return BINT(res);         
+ }
+ const BINT operator*(const BINT &t) 
+ {
+  string sa = s, sb = t.s;
+  int la = sa.size(), lb = sb.size();
+  int m = la + lb;
+  vector<int>a(m + 1, 0), b(m + 1, 0), c(m + 1, 0);
+  for (int i = 0; i < la; i++) a[i] = sa[la - 1 - i] - '0';
+  for (int i = 0; i < lb; i++) b[i] = sb[lb - 1 - i] - '0';
+  for (int i = 0; i < la; i++)
+   for (int j = 0; j < lb; j++)
+    c[i + j] += a[i] * b[j], c[i + j + 1] += c[i + j] / 10, c[i + j] %= 10;
+  while (!c[m] && m) m--;
+  string res;
+  for (int i = m; i >= 0; i--)
+   res += char(c[i] + '0');
+  return BINT(res);
+ }
+ bool operator<(const BINT &t) {
+  string b = t.s;
+  if (s.size() < b.size()) return true;
+  if (s.size() == b.size() && s < b) return true;
+  return false;
+ }
+ void operator=(const string &x) { s = x; }
+ void operator=(const long long &x) { s = to_string(x); }
+ void operator=(const int &x) { s = to_string(x); }
+ friend istream& operator>>(istream & in, BINT & t) {
+  in >> t.s;         
+  return in;
+ }
+ friend ostream& operator<<(ostream & out, const BINT & t) {
+  out << t.s;      
+  return out;        
+ }
+};
+signed main(){return 0;}
