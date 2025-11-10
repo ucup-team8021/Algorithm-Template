08459779736570e7d8b9f1b81f8c70cf9f5a8385
Tau835 <FN_AK_IOI@outlook.com>
2025-11-10 18:51:25 +0800

    谁把快速幂写错了??????

--- 数学Math/binary-exponentiation.cpp
+++ 数学Math/binary-exponentiation.cpp
@@ -1,12 +1,8 @@
 // {"name": "快速幂", "intro": "在 $O(\log n)$ 的时间内求出 $a^n \bmod p$ 的值。"}
 using ll = long long;
-inline ll qpow(ll a, ll b, ll mod) {
-    ll res = 1;
-    a %= mod;
-    if (b) {
-        if (b & 1) res = res * a % mod;
-        a = a * a % mod, b >>= 1;
-    }
-    return res % mod;
+ll qpow(ll a, ll b, ll M) {
+  ll ret = 1;
+  for (a %= M; b; b >>= 1, a = a * a % M)
+    if (b & 1) ret = ret * a % M;
+  return ret;
 }
-int main() { return 0; }
